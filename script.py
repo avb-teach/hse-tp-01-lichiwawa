b804e1465caedb8d605efc33da35df4addb03c23
root <root@rwvpiwqvzl.local>
2025-04-28 10:17:49 +0000

    test5

--- script.py
+++ script.py
@@ -6,5 +6,7 @@ input_dir = sys.argv[1]
 output_dir = sys.argv[2]
 for dirpath, dirnames, filenames in os.walk(input_dir):
     for filename in filenames:
-        shutil.move(filename, output_dir)
+        src_path = os.path.join(dirpath, filename)
+        dst_path = os.path.join(output_dir, filename)
+        shutil.copyfile(src_path, dst_path)
         

ba0b16e1731e2ea720b18a00b7fe6edd455b1598
root <root@rwvpiwqvzl.local>
2025-04-28 11:25:46 +0000

    test6

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ import shutil
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
+#цикл с функцией обхода взят из видео: https://www.youtube.com/watch?v=tJxcKyFMTGo&t=339s
 for dirpath, dirnames, filenames in os.walk(input_dir):
     for filename in filenames:
         src_path = os.path.join(dirpath, filename)

2af2d575a76a985753d5028f2ffa07e6797ec4ef
root <root@rwvpiwqvzl.local>
2025-04-28 07:43:29 +0000

    test3

---
+++ collect_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 script.py $1 $2
+#передача аргументов взята с сайта: https://linuxconfig.org/how-to-use-a-bash-script-to-run-your-python-scripts#:~:text=Passing%20arguments%20to%20Python%20scripts%20from%20a%20bash%20script,-We%20can%20pass&text=In%20this%20script%2C%20we%20run,%2C%20and%20%243%20
---
+++ script.py
@@ -0,0 +1,5 @@
+import sys
+import os
+
+input_dir = sys.argv[1]
+output_dir = sys.argv[2]

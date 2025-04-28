b99c23c0c9b0cc392c24058c618344c0a06e5af6
root <root@rwvpiwqvzl.local>
2025-04-28 08:38:17 +0000

    test4

--- script.py
+++ script.py
@@ -1,5 +1,10 @@
 import sys
 import os
+import shutil
 
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
+for dirpath, dirnames, filenames in os.walk(input_dir):
+    for filename in filenames:
+        shutil.move(filename, output_dir)
+        
